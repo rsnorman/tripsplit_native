3e7a0c1377c0b26dbe6998589f14a3617623af00
Ryan Norman <rsnorman15@gmail.com>
2017-04-20 21:09:58 -0400

    Resets navigation after logout

--- reducers/root_navigation_reducer.js
+++ reducers/root_navigation_reducer.js
@@ -1,6 +1,10 @@
 import { NavigationActions } from 'react-navigation';
 import RootNavigator from './../components/RootNavigator';
 
+function onHomeScreen(state) {
+  return !state.routes || !state.routes[0] || state.routes[0].index === 0;
+}
+
 /**
  * Export the reducer for the root navigator.
  */
@@ -8,6 +12,11 @@ const rootNavigation = (state, action) => {
   switch (action.type) {
     case 'CREATE_ACCOUNT':
     case 'CREATE_SESSION':
+    case 'DESTROY_ACCOUNT':
+    case 'DESTROY_SESSION':
+      if (onHomeScreen(state)) {
+        return RootNavigator.router.getStateForAction(action, state);
+      }
       return RootNavigator.router.getStateForAction(NavigationActions.navigate({ routeName: 'Home' }), state);
     case 'OPEN_DRAWER':
       return RootNavigator.router.getStateForAction(NavigationActions.navigate({ routeName: 'DrawerOpen' }), state);
--- reducers/session_nav_reducer.js
+++ reducers/session_nav_reducer.js
@@ -3,15 +3,13 @@ import SessionStackNavigator from './../components/SessionStackNavigator';
 
 const sessionNav = (state, action) => {
   switch (action.type) {
+    case 'DESTROY_ACCOUNT':
     case 'DESTROY_SESSION':
-      const resetAction = NavigationActions.reset({
-        index: 0,
-        actions: [
-          NavigationActions.navigate({ routeName: 'NewSession'}),
-          NavigationActions.navigate({ routeName: 'NewAccount'})
-        ]
-      });
-      return SessionStackNavigator.router.getStateForAction(resetAction, state);
+      if (state.index === 0) {
+        return SessionStackNavigator.router.getStateForAction(action, state);
+      } else {
+        return SessionStackNavigator.router.getStateForAction(NavigationActions.back(), state);
+      }
     case 'VIEW_REGISTRATION':
       return SessionStackNavigator.router.getStateForAction(NavigationActions.navigate({ routeName: 'NewAccount' }), state);
     case 'VIEW_LOGIN':
